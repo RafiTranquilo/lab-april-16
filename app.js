a96975c0df7699515d3f49986159657654f30b76
JazzyMcSuperfly <JazzyMcSupefly@gmail.com>
2016-04-15 14:51:10 -0700

    adds problem 3 to app.js - cleans up and comments on problems 1 and 2

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
+// - PROBLEM 1
 function sum(a, b) {
-  console.log('The sum of ' + a + ' and ' + b + ' is ' + (a + b));
+  // console.log('The sum of ' + a + ' and ' + b + ' is ' + sum(a, b));
+  return a + b;
 }
 
+// - PROBLEM 2
 function multiply(c, d) {
-  console.log('The product of ' + c + ' and ' + d + ' is ' + (c * d));
+  // console.log('The product of ' + c + ' and ' + d + ' is ' + (c * d));
+  return c * d;
+}
+
+// - PROBLEM 3
+function sumAndMultiply(e, f, g) {
+  var output = [];
+  output.push(sum(sum(e, f), g));
+  output.push(multiply(multiply(e, f), g));
+  console.log(e + ' and ' + f + ' and ' + g + ' sum to ' + output[0]);
+  console.log('The numbers ' + e + ' and ' + f + ' and ' + g + ' have a product of ' + output[1]);
+  return output;
 }
