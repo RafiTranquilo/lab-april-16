a16ada3696347926ac8eaf562cb5b08dd78e7ca5
JazzyMcSuperfly <JazzyMcSupefly@gmail.com>
2016-04-15 13:42:23 -0700

    adds "multiply" function to app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function sum(a, b) {
   console.log('The sum of ' + a + ' and ' + b + ' is ' + (a + b));
 }
+
+function multiply(c, d) {
+  console.log('The product of ' + c + ' and ' + d + ' is ' + (c * d));
+}
